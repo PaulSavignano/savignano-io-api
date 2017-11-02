aabb3f840821e1b1d088c61b65e99b28ee91c7de
Paul Savignano <Paul.Savignano@gmail.com>
2017-11-02 14:07:01 -0700

    Add console.log(process.env.NODE_ENV)

--- api/config/config.js
+++ api/config/config.js
@@ -1,5 +1,8 @@
 const env = process.env.NODE_ENV || 'development'
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+console.log('env', env)
+
 if (env === 'development' || env === 'test') {
   const config = require('../../config.json')
   const envConfig = config[env]
