2ea4f14079c69e272a3d70b76e4dfefe659e83df
Paul Savignano <Paul.Savignano@gmail.com>
2018-04-03 10:07:13 -0700

    Debug

--- api/moverbase/controllers/moverbase.js
+++ api/moverbase/controllers/moverbase.js
@@ -8,10 +8,12 @@ export const requestEstimate = async (req, res) => {
     body,
     params: { brandName }
   } = req
-  const moverbaseApiKey = await ApiConfig.findOne({ brandName })
-  if (!moverbaseApiKey) throw 'Sorry, there was no moverbase api key found'
-  const auth = 'Basic ' + new Buffer(moverbaseApiKey.values.moverbaseKey + ':').toString('base64')
   try {
+    const moverbaseApiKey = await ApiConfig.findOne({ brandName })
+    if (!moverbaseApiKey) throw 'Sorry, there was no moverbase api key found'
+    const auth = 'Basic ' + new Buffer(moverbaseApiKey.values.moverbaseKey + ':').toString('base64')
+    console.log('moverbase api key: ', moverbaseApiKey.values.moverbaseKey)
+    console.log('moverbase auth: ', auth)
     const response = await fetch(`https://api.moverbase.com/v1/leads/`, {
       method: 'POST',
       headers: {
@@ -22,18 +24,19 @@ export const requestEstimate = async (req, res) => {
     })
     console.log('moverbase res: ', response)
     const json = await response.json()
+    console.log('moverbase json: ', json)
     const emailInfo = await sendGmail({
       brandName,
-      to: email,
+      to: body.email,
       toSubject: 'Thank you for contacting us for a free estimate',
-      name: firstName,
+      name: body.firstName,
       toBody: `<p>Thank you for requesting a free estimate.  We will contact you shortly!</p>`,
       fromSubject: `New Estimate Request`,
       fromBody: `
-        <p>${firstName} just contacted you through ${brandName}.</p>
-        ${phone && `<div>Phone: ${phone}</div>`}
-        <div>Email: ${email}</div>
-        <div>Note: ${note}</div>
+        <p>${bod.firstName} just contacted you through ${brandName}.</p>
+        ${body.phone && `<div>Phone: ${body.phone}</div>`}
+        <div>Email: ${body.email}</div>
+        <div>Note: ${body.note}</div>
       `
     })
     res.status(200).send()
