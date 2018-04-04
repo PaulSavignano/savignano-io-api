289c1e951302ae4a334baf896d4c8209384b229f
Paul Savignano <Paul.Savignano@gmail.com>
2018-04-03 20:53:51 -0700

    Refac headers

--- api/index.js
+++ api/index.js
@@ -29,9 +29,9 @@ const port = process.env.PORT
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, cart")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, cart, Authorization")
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-  res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token, cart')
+  res.header('Access-Control-Expose-Headers', 'x-access-token, x-refresh-token, cart, Authorization')
   next()
 })
 
