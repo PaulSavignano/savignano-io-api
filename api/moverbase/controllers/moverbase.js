63932140cfbce3f40c49e2239700b6491dd880b3
Paul Savignano <Paul.Savignano@gmail.com>
2018-04-03 20:19:54 -0700

    Refactor moverbase

--- api/moverbase/controllers/moverbase.js
+++ api/moverbase/controllers/moverbase.js
@@ -10,8 +10,10 @@ export const requestEstimate = async (req, res) => {
   } = req
   try {
     const moverbaseApiKey = await Config.findOne({ appName })
+    console.log('moverbaseApiKey: ', moverbaseApiKey)
     if (!moverbaseApiKey) throw 'Sorry, there was no moverbase api key found'
     const auth = `Basic ${new Buffer(moverbaseApiKey + ':').toString('base64')}`
+    console.log('auth: ', auth)
     const response = await fetch(`https://api.moverbase.com/v1/leads/`, {
       method: 'POST',
       headers: {
@@ -35,7 +37,7 @@ export const requestEstimate = async (req, res) => {
     console.log('json: ', json)
     res.status(200).send()
   } catch (error) {
-    console.error(error)
+    console.error('Error is: ', error)
     res.status(400).send({ error })
   }
 }
